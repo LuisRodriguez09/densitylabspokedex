f911435985d91ce8ec309fdc00f48a1f5847c956
Luis Galindo <bits0048@bray.com>
2023-06-21 22:17:51 -0600

    tres

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { useAppSelector } from "./redux/hooks";
 function App() {
   const { isLoading } = useAppSelector((state) => state.pokemons);
 
-  const test;
+  const otrotest;
 
   return (
     <BrowserRouter>
