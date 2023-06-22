54a05707660ec77a9d9c353a1dde01c617d902cc
Luis Galindo <bits0048@bray.com>
2023-06-21 22:49:56 -0600

    Hustky

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,6 @@ import { useAppSelector } from "./redux/hooks";
 function App() {
   const { isLoading } = useAppSelector((state) => state.pokemons);
 
-  const num = 0
-  if(num == 0){
-    console.log("XD")
-  }
-
   return (
     <BrowserRouter>
       {isLoading && <Loading />}
